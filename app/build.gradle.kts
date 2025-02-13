300e7b110c742b36b119742f2073ca0f7ef2d901
t1nm1ksun <minsuk07672@naver.com>
2025-02-13 22:05:19 +0900

    chore#3 : minimum Sdk 수정

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,7 +20,7 @@ android {
 
     defaultConfig {
         applicationId = "com.depromeet.team6"
-        minSdk = 28
+        minSdk = 26
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
