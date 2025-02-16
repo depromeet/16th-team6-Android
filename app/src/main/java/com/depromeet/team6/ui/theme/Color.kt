1d7278d7c4c7fc58804df8f2e21c9d86e9c18f3f
cyjadela <adelacho@naver.com>
2025-02-16 17:57:31 +0900

    design/#7 : color 초기 설정

--- app/src/main/java/com/depromeet/team6/ui/theme/Color.kt
+++ app/src/main/java/com/depromeet/team6/ui/theme/Color.kt
@@ -9,3 +9,26 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+// Common
+val White = Color(0xFFFFFFFF)
+val Black = Color(0xFF000000)
+
+// Text
+val GreySecondaryLabel = Color(0xFF999CA4)
+val GreyTertiaryLabel = Color(0xFF666970)
+val GreyQuaternaryLabel = Color(0xFF393C42)
+val GreyLink = Color(0xFF4C4D53)
+
+// Background
+val GreyElevatedBackground = Color(0xFF1C1C1D)
+val GreyWashBackground = Color(0xFF18181A)
+val GreyDivider = Color(0xFF4D4D4D)
+
+// Card
+val GreyCard = Color(0xFF1C1C1E)
+val GreyElevatedCard = Color(0xFF2C2C2E)
+
+// Button
+val GreyButton = Color(0xFF29292C)
+val GreyButtonOutline = Color(0xFF36363A)
