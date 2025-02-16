00f65df848a439b2c13eb1e879b941be5b488eea
cyjadela <adelacho@naver.com>
2025-02-16 18:03:57 +0900

    design/#7 : font, color 적용 테스트

--- app/src/main/java/com/depromeet/team6/MainActivity.kt
+++ app/src/main/java/com/depromeet/team6/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -11,7 +12,9 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.depromeet.team6.ui.theme.GreyLink
 import com.depromeet.team6.ui.theme.Team6Theme
+import com.depromeet.team6.ui.theme.bodySemiBold12
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -32,10 +35,18 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "슬랙 알림 진짜 안가용가리?! ㅠㅠ",
-        modifier = modifier
-    )
+    Column {
+        Text(
+            text = "슬랙 알림 진짜 안가용가리?! ㅠㅠ",
+            modifier = modifier
+        )
+        Text(
+            text = "슬랙 알림 이제 와용가리 ~ + 컬러, 폰트 적용 테스트",
+            modifier = modifier,
+            color = GreyLink,
+            style = bodySemiBold12
+        )
+    }
 }
 
 @Preview(showBackground = true)
