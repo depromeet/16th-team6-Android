b412ff888fb23b37f39e14cb48b3d4cd147a8756
Jieun <lje2000lje@naver.com>
2025-02-24 22:36:16 +0900

    feat/#16 : 지도에 현위치 마커 추가

--- app/src/main/java/com/depromeet/team6/presentation/ui/home/component/MapView.kt
+++ app/src/main/java/com/depromeet/team6/presentation/ui/home/component/MapView.kt
@@ -2,44 +2,97 @@ package com.depromeet.team6.presentation.ui.home.component
 
 import android.util.Log
 import android.widget.FrameLayout
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
+import androidx.core.content.ContextCompat
+import androidx.core.graphics.drawable.toBitmap
 import com.depromeet.team6.BuildConfig
+import com.depromeet.team6.R
+import com.google.android.gms.maps.model.LatLng
+import com.skt.tmap.TMapPoint
 import com.skt.tmap.TMapView
+import com.skt.tmap.overlay.TMapMarkerItem
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 
 @Composable
 fun TMapViewCompose(
+    currentLocation: LatLng,
     modifier: Modifier = Modifier
 ) {
     val context = LocalContext.current
     val tMapView = remember { TMapView(context) }
+    var isMapReady by remember { mutableStateOf(false) }
 
     LaunchedEffect(Unit) {
         tMapView.setSKTMapApiKey(BuildConfig.TMAP_API_KEY)
         tMapView.setOnMapReadyListener {
             tMapView.mapType = TMapView.MapType.NIGHT
+            isMapReady = true
         }
     }
 
-    AndroidView(
-        modifier = modifier.fillMaxSize(),
-        factory = { context ->
-            // FrameLayout을 직접 생성
-            FrameLayout(context).apply {
-                // TMapView를 FrameLayout에 추가
-                addView(tMapView)
+    // 현재 위치 변경될 때만 마커 갱신
+    LaunchedEffect(currentLocation, isMapReady) {
+        if (isMapReady) {
+            val tMapPoint = TMapPoint(currentLocation.latitude, currentLocation.longitude)
+
+            withContext(Dispatchers.Main) {
+
+                tMapView.setCenterPoint(tMapPoint.latitude, tMapPoint.longitude)
+                tMapView.zoomLevel = 18
+
+                val markerDrawable =
+                    ContextCompat.getDrawable(context, R.drawable.ic_home_current_location)
+                val markerBitmap = markerDrawable?.toBitmap()
+
+                val markerItem = TMapMarkerItem().apply {
+                    id = "CurrentMarker"
+                    name = "Current Location"
+                    icon = markerBitmap
+                    setTMapPoint(tMapPoint)
+                }
+
+                tMapView.addTMapMarkerItem(markerItem)
             }
-        },
-        update = { frameLayout ->
-            // Update logic if needed (e.g., map settings)
         }
-    )
+    }
+
+    Box(
+        modifier = modifier.fillMaxSize()
+    ) {
+        AndroidView(
+            modifier = modifier.fillMaxSize(),
+            factory = { context ->
+                // FrameLayout을 직접 생성
+                FrameLayout(context).apply {
+                    // TMapView를 FrameLayout에 추가
+                    addView(tMapView)
+                }
+            },
+            update = { frameLayout ->
+                // Update logic if needed (e.g., map settings)
+            }
+        )
+    }
 
     DisposableEffect(Unit) {
         onDispose {
