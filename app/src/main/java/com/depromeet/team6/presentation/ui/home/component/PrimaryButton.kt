a26bacb1daf13115acf0e426d7e3e202cc9a7992
Jieun <lje2000lje@naver.com>
2025-03-01 23:24:36 +0900

    feat/#16 : height 고정값 padding으로 변경

--- app/src/main/java/com/depromeet/team6/presentation/ui/home/component/PrimaryButton.kt
+++ app/src/main/java/com/depromeet/team6/presentation/ui/home/component/PrimaryButton.kt
@@ -4,7 +4,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
@@ -36,7 +36,7 @@ fun PrimaryButton(
         onClick = onClick,
         modifier = modifier
             .fillMaxWidth()
-            .height(52.dp),
+            .padding(vertical = 14.dp),
         colors = ButtonDefaults.buttonColors(
             containerColor = colors.systemGreen,
             contentColor = colors.black
