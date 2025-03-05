5fb745317cfbcc74dd6fbc6c26cae5c3b5266718
hyuns <hyuns6677@gmail.com>
2025-03-06 03:31:02 +0900

    feat#12 : 경로 검색창에서 길이초과시 elipsize 처리

--- app/src/main/java/com/depromeet/team6/presentation/ui/course/component/DestinationSearchBar.kt
+++ app/src/main/java/com/depromeet/team6/presentation/ui/course/component/DestinationSearchBar.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -14,6 +15,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.depromeet.team6.R
@@ -39,9 +41,14 @@ fun DestinationSearchBar(
     ) {
         // TODO : typography 디자인 시스템 적용
         Text(
+            modifier = Modifier
+                .wrapContentWidth()
+                .weight(1f, fill = false),
             text = startingPoint,
             style = defaultTeam6Typography.bodyRegular15,
-            color = defaultTeam6Colors.white
+            color = defaultTeam6Colors.white,
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis
         )
 
         Image(
@@ -65,5 +72,14 @@ fun PreviewDestinationSearchBar() {
     DestinationSearchBar(
         startingPoint = "마루 180",
         destination = "집"
+    )
+}
+
+@Preview(name = "overflow")
+@Composable
+fun PreviewDestinationSearchBarOverflow() {
+    DestinationSearchBar(
+        startingPoint = "마루 킁킁 마루 쫑긋 마루 덥석 총총총총총 짧은 다리 파다닥 마루 폴짝 마루 까딱 마루 꼴깍 총총총총총 언니를 따라 가요",
+        destination = "기숙사"
     )
 }
