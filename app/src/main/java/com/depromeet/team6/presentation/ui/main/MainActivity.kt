cb911a68c2b7970e3124ec2612c80ca811dd78eb
t1nm1ksun <minsuk07672@naver.com>
2025-03-07 16:58:38 +0900

    chore#14 : ktlint Format

--- app/src/main/java/com/depromeet/team6/presentation/ui/main/MainActivity.kt
+++ app/src/main/java/com/depromeet/team6/presentation/ui/main/MainActivity.kt
@@ -6,7 +6,6 @@ import android.util.Log
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -15,7 +14,6 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -23,14 +21,13 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import com.depromeet.team6.presentation.ui.login.LoginRoute
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.depromeet.team6.R
 import com.depromeet.team6.data.datalocal.manager.LockServiceManager
 import com.depromeet.team6.data.datalocal.permission.PermissionUtil
-import com.depromeet.team6.presentation.ui.course.CourseScreen
+import com.depromeet.team6.presentation.ui.login.LoginRoute
 import com.depromeet.team6.ui.theme.Team6Theme
 import com.google.android.gms.tasks.OnCompleteListener
 import com.google.firebase.messaging.FirebaseMessaging
@@ -52,29 +49,31 @@ class MainActivity : ComponentActivity() {
 
         setContent {
             Team6Theme {
+                val snackbarHostState = remember { SnackbarHostState() }
+                val coroutineScope = rememberCoroutineScope()
+
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     LoginRoute(
                         modifier = Modifier.padding(innerPadding),
                         navigateToOnboarding = {},
                         navigateToHome = {}
                     )
-                val snackbarHostState = remember { SnackbarHostState() }
-                val coroutineScope = rememberCoroutineScope()
-            }
-        }
-
-        FirebaseMessaging.getInstance().token.addOnCompleteListener(
-            OnCompleteListener { task ->
-                if (!task.isSuccessful) {
-                    Log.d(TAG, "Fetching FCM registration token failed")
-                    return@OnCompleteListener
                 }
-
-                val token = task.result
-
-                Log.d("Fcm Token", token)
             }
-        )
+
+            FirebaseMessaging.getInstance().token.addOnCompleteListener(
+                OnCompleteListener { task ->
+                    if (!task.isSuccessful) {
+                        Log.d(TAG, "Fetching FCM registration token failed")
+                        return@OnCompleteListener
+                    }
+
+                    val token = task.result
+
+                    Log.d("Fcm Token", token)
+                }
+            )
+        }
     }
 
     private fun startLockService() {
