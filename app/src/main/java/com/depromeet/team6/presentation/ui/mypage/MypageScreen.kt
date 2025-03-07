17624f1aa11e42c7023e9ef450aa8c3dd5210455
cyjadela <adelacho@naver.com>
2025-03-08 08:05:55 +0900

    feat/#37 : mypage 리스트 컴포넌트 추가

---
+++ app/src/main/java/com/depromeet/team6/presentation/ui/mypage/MypageScreen.kt
@@ -0,0 +1,17 @@
+package com.depromeet.team6.presentation.ui.mypage
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+fun MypageScreen(
+    modifier: Modifier = Modifier
+) {
+
+}
+
+@Preview
+@Composable
+fun MypageScreenPreview() {
+    MypageScreen()
+}
