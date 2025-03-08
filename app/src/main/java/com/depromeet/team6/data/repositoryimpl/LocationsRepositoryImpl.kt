22708bec49caec74b834a95d5e8508ca09a2ad23
Jieun <lje2000lje@naver.com>
2025-03-09 02:22:36 +0900

    feat/#42 : getAddressFromCoordinates 레포지토리 함수 구현

--- app/src/main/java/com/depromeet/team6/data/repositoryimpl/LocationsRepositoryImpl.kt
+++ app/src/main/java/com/depromeet/team6/data/repositoryimpl/LocationsRepositoryImpl.kt
@@ -2,6 +2,7 @@ package com.depromeet.team6.data.repositoryimpl
 
 import com.depromeet.team6.data.dataremote.datasource.LocationsRemoteDataSource
 import com.depromeet.team6.data.mapper.todomain.toDomain
+import com.depromeet.team6.domain.model.Address
 import com.depromeet.team6.domain.model.Location
 import com.depromeet.team6.domain.repository.LocationsRepository
 import javax.inject.Inject
@@ -9,7 +10,15 @@ import javax.inject.Inject
 class LocationsRepositoryImpl @Inject constructor(
     private val locationsRemoteDataSource: LocationsRemoteDataSource
 ) : LocationsRepository {
-    override suspend fun getLocations(keyword: String, lat: Double, lon: Double): Result<List<Location>> =
+    override suspend fun getLocations(
+        keyword: String,
+        lat: Double,
+        lon: Double
+    ): Result<List<Location>> =
         locationsRemoteDataSource.getLocations(keyword = keyword, lat = lat, lon = lon)
+            .mapCatching { it.toDomain() }
+
+    override suspend fun getAddressFromCoordinates(lat: Double, lon: Double): Result<Address> =
+        locationsRemoteDataSource.getAddressFromCoordinates(lat = lat, lon = lon)
             .mapCatching { it.toDomain() }
 }
