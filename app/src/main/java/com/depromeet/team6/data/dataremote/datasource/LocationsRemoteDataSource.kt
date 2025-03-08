6fc6d008c4aad49db2215b089eb1fe5c37becb6f
Jieun <lje2000lje@naver.com>
2025-03-09 02:22:46 +0900

    feat/#42 : getAddressFromCoordinates 데이터 소스 구현

--- app/src/main/java/com/depromeet/team6/data/dataremote/datasource/LocationsRemoteDataSource.kt
+++ app/src/main/java/com/depromeet/team6/data/dataremote/datasource/LocationsRemoteDataSource.kt
@@ -1,6 +1,7 @@
 package com.depromeet.team6.data.dataremote.datasource
 
 import com.depromeet.team6.data.dataremote.model.response.base.toResult
+import com.depromeet.team6.data.dataremote.model.response.locations.ResponseAddressDto
 import com.depromeet.team6.data.dataremote.model.response.locations.ResponseLocationsDto
 import com.depromeet.team6.data.dataremote.service.LocationsService
 import javax.inject.Inject
@@ -10,4 +11,7 @@ class LocationsRemoteDataSource @Inject constructor(
 ) {
     suspend fun getLocations(keyword: String, lat: Double, lon: Double): Result<List<ResponseLocationsDto>> =
         locationsService.getLocations(keyword = keyword, lat = lat, lon = lon).toResult()
+
+    suspend fun getAddressFromCoordinates(lat: Double, lon: Double): Result<ResponseAddressDto> =
+        locationsService.getAddressFromCoordinates(lat = lat, lon = lon).toResult()
 }
