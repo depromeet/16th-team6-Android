e5e537e0cc97c52e385591d161ae28e3da0f07d3
Jieun <lje2000lje@naver.com>
2025-03-09 02:21:44 +0900

    feat/#42 : ResponseAddressDto 생성

---
+++ app/src/main/java/com/depromeet/team6/data/dataremote/model/response/locations/ResponseAddressDto.kt
@@ -0,0 +1,14 @@
+package com.depromeet.team6.data.dataremote.model.response.locations
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class ResponseAddressDto(
+    @SerialName("name")
+    val name: String,
+    @SerialName("lat")
+    val lat: Double,
+    @SerialName("lon")
+    val lon: Double
+)
