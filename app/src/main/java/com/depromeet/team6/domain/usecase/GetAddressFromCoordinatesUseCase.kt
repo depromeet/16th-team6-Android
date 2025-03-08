d0b0f48f837608c8bd17ad834b85691e0e2c4d35
Jieun <lje2000lje@naver.com>
2025-03-09 02:23:09 +0900

    feat/#42 : GetAddressFromCoordinatesUseCase 유즈케이스 구현

---
+++ app/src/main/java/com/depromeet/team6/domain/usecase/GetAddressFromCoordinatesUseCase.kt
@@ -0,0 +1,14 @@
+package com.depromeet.team6.domain.usecase
+
+import com.depromeet.team6.domain.model.Address
+import com.depromeet.team6.domain.repository.LocationsRepository
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class GetAddressFromCoordinatesUseCase @Inject constructor(
+    private val locationsRepository: LocationsRepository
+){
+    suspend operator fun invoke(lat: Double, lon: Double): Result<Address> =
+        locationsRepository.getAddressFromCoordinates(lat = lat, lon = lon)
+}
