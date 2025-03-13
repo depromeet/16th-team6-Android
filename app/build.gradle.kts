681a0b0e07e9ed9c4fe14aba194202e79f5e10b0
hyuns <hyuns6677@gmail.com>
2025-03-13 19:12:35 +0900

    feat#44 : 디버그용 빌드 분리
    - 앗차차!! 가 디버그용 앱 입니다.
    - project 보기에서 debug, release 분기 디렉토리 만들었습니다.
    해당 디렉토리안에 google-service.json이랑 strings.xml 파일 나눠서 빌드됩니다.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,7 +23,7 @@ android {
         applicationId = "com.depromeet.team6"
         minSdk = 26
         targetSdk = 34
-        versionCode = 1
+        versionCode = 2
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
@@ -44,6 +44,8 @@ android {
         }
         debug {
             isMinifyEnabled = false
+            isDebuggable = true
+            applicationIdSuffix = ".debug"
             buildConfigField("String", "BASE_URL", properties["dev.base.url"].toString())
         }
     }
