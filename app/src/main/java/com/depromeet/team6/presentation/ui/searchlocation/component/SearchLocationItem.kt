3a50eaa32e55d35470ad58bfb7fda739562119d0
Jieun <lje2000lje@naver.com>
2025-03-16 18:26:15 +0900

    feat/#57 : 검색 리스트 텍스트 분리 및 아이콘 추가

--- app/src/main/java/com/depromeet/team6/presentation/ui/searchlocation/component/SearchLocationItem.kt
+++ app/src/main/java/com/depromeet/team6/presentation/ui/searchlocation/component/SearchLocationItem.kt
@@ -1,23 +1,26 @@
 package com.depromeet.team6.presentation.ui.searchlocation.component
 
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.depromeet.team6.R
 import com.depromeet.team6.domain.model.HomeSearchLocation
 import com.depromeet.team6.presentation.util.modifier.noRippleClickable
-import com.depromeet.team6.presentation.util.modifier.roundedBackgroundWithPadding
 import com.depromeet.team6.ui.theme.defaultTeam6Colors
 import com.depromeet.team6.ui.theme.defaultTeam6Typography
 
@@ -46,25 +49,36 @@ fun SearchLocationItem(
 
             Spacer(modifier = Modifier.height(4.dp))
 
-            Text(
-                text = homeSearchLocation.distance + "" + homeSearchLocation.roadAddress,
-                style = defaultTeam6Typography.bodyRegular14,
-                color = defaultTeam6Colors.greySecondaryLabel,
-                overflow = TextOverflow.Ellipsis
-            )
-        }
-        Spacer(modifier = Modifier.width(12.dp))
-        Text(
-            text = "선택",
-            color = defaultTeam6Colors.white,
-            style = defaultTeam6Typography.bodyMedium13,
-            modifier = Modifier
-                .roundedBackgroundWithPadding(
-                    backgroundColor = defaultTeam6Colors.greyButtonOutline,
-                    cornerRadius = 8.dp,
-                    padding = PaddingValues(vertical = 8.dp, horizontal = 12.5.dp)
+            Row(
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(
+                    text = homeSearchLocation.distance,
+                    style = defaultTeam6Typography.bodyRegular14,
+                    color = defaultTeam6Colors.greySecondaryLabel,
+                    overflow = TextOverflow.Ellipsis
                 )
-        )
+
+                Spacer(Modifier.width(6.dp))
+
+                Icon(
+                    imageVector = ImageVector.vectorResource(R.drawable.ic_home_location_grey_3dp),
+                    contentDescription = stringResource(R.string.home_search_dot_icon),
+                    tint = defaultTeam6Colors.white
+                )
+
+                Spacer(Modifier.width(6.dp))
+
+                Text(
+                    text = homeSearchLocation.roadAddress,
+                    style = defaultTeam6Typography.bodyRegular14,
+                    color = defaultTeam6Colors.greySecondaryLabel,
+                    overflow = TextOverflow.Ellipsis
+                )
+            }
+        }
+
+        Spacer(modifier = Modifier.width(12.dp))
     }
 }
 
@@ -73,7 +87,7 @@ fun SearchLocationItem(
 private fun SearchLocationItemPreview() {
     SearchLocationItem(
         homeSearchLocation = HomeSearchLocation(
-            name = "60계 치킨 강남정",
+            name = "60계 치킨 강남점",
             distance = "1.9km",
             roadAddress = "강남구 테헤란로 4길 6"
         )
