de7f2dca5772313deae2ee619d6f87aadcb08b3f
t1nm1ksun <minsuk07672@naver.com>
2025-03-20 18:12:59 +0900

    feat#61 : 위치검색 텍스트필드 UI 및 포커스 로직 구현

--- app/src/main/java/com/depromeet/team6/presentation/ui/onboarding/component/OnboardingSearchPopup.kt
+++ app/src/main/java/com/depromeet/team6/presentation/ui/onboarding/component/OnboardingSearchPopup.kt
@@ -5,17 +5,21 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.depromeet.team6.domain.model.Location
 import com.depromeet.team6.presentation.ui.onboarding.OnboardingContract
+import com.depromeet.team6.presentation.util.modifier.addFocusCleaner
 import com.depromeet.team6.ui.theme.defaultTeam6Colors
 
 @Composable
@@ -25,38 +29,38 @@ fun OnboardingSearchPopup(
     uiState: OnboardingContract.OnboardingUiState = OnboardingContract.OnboardingUiState(),
     searchText: String = "",
     onSearchTextChange: (String) -> Unit = {},
-    onCloseButtonClicked: () -> Unit = {},
+    onBackButtonClicked: () -> Unit = {},
+    onTextClearButtonClicked: () -> Unit = {},
     selectButtonClicked: (Location) -> Unit = {}
 ) {
+    val focusManager = LocalFocusManager.current
+    val focusRequester = remember { FocusRequester() }
     Column(
         modifier = modifier
             .padding(padding)
             .fillMaxSize()
+            .addFocusCleaner(focusManager)
             .background(color = defaultTeam6Colors.greyWashBackground)
     ) {
         OnboardingSearchTextField(
             value = searchText,
             onValueChange = onSearchTextChange,
-            onCloseButtonClicked = onCloseButtonClicked
+            onBackButtonClicked = onBackButtonClicked,
+            onTextClearButtonClicked = onTextClearButtonClicked,
+            focusRequester = focusRequester
         )
-        Spacer(modifier = Modifier.height(10.dp))
-
-        if (uiState.searchLocations.isNotEmpty()) {
-            Spacer(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .height(1.dp)
-                    .background(color = defaultTeam6Colors.greyDivider)
-            )
-            LazyColumn {
-                items(uiState.searchLocations) { onboardingSearchLocation ->
-                    OnboardingSearchLocationItem(
-                        onboardingSearchLocation = onboardingSearchLocation,
-                        selectButtonClicked = { selectButtonClicked(onboardingSearchLocation) }
-                    )
-                }
+        Spacer(modifier = Modifier.height(4.dp))
+        LazyColumn {
+            items(uiState.searchLocations) { onboardingSearchLocation ->
+                OnboardingSearchLocationItem(
+                    onboardingSearchLocation = onboardingSearchLocation,
+                    selectButtonClicked = { selectButtonClicked(onboardingSearchLocation) }
+                )
             }
         }
+    }
+    LaunchedEffect(Unit) {
+        focusRequester.requestFocus()
     }
 }
 
