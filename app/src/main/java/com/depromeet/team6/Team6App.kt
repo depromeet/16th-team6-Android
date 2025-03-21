4031c4293f3da51b71f10760492458f37cb095a7
t1nm1ksun <minsuk07672@naver.com>
2025-03-21 15:57:48 +0900

    chore#54 : 로그 팀버로 변경

--- app/src/main/java/com/depromeet/team6/Team6App.kt
+++ app/src/main/java/com/depromeet/team6/Team6App.kt
@@ -4,7 +4,6 @@ import android.app.Application
 import android.content.pm.PackageManager
 import android.os.Build
 import android.util.Base64
-import android.util.Log
 import androidx.appcompat.app.AppCompatDelegate
 import com.depromeet.team6.BuildConfig.KAKAO_NATIVE_APP_KEY
 import com.kakao.sdk.common.KakaoSdk
@@ -33,16 +32,16 @@ class Team6App : Application() {
     }
 
     private fun getKeyHash() {
-        Log.d("getKeyHash", "key hash: ${Utility.getKeyHash(this)}")
+        Timber.d("getKeyHash key hash: ${Utility.getKeyHash(this)}")
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
             val packageInfo = this.packageManager.getPackageInfo(this.packageName, PackageManager.GET_SIGNING_CERTIFICATES)
             for (signature in packageInfo.signingInfo?.apkContentsSigners!!) {
                 try {
                     val md = MessageDigest.getInstance("SHA")
                     md.update(signature.toByteArray())
-                    Log.d("getKeyHash", "key hash: ${Base64.encodeToString(md.digest(), Base64.NO_WRAP)}")
+                    Timber.d("getKeyHash key hash: ${Base64.encodeToString(md.digest(), Base64.NO_WRAP)}")
                 } catch (e: NoSuchAlgorithmException) {
-                    Log.w("getKeyHash", "Unable to get MessageDigest. signature=$signature", e)
+                    Timber.w("getKeyHash Unable to get MessageDigest. signature=$signature", e)
                 }
             }
         }
