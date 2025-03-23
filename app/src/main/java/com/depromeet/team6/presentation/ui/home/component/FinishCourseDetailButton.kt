688363f78b8458117818459cc26d39473d99acd7
cyjadela <adelacho@naver.com>
2025-03-23 10:46:23 +0900

    feat/#63 : 홈 알림 등록 후 버튼 string 추가

--- app/src/main/java/com/depromeet/team6/presentation/ui/home/component/FinishCourseDetailButton.kt
+++ app/src/main/java/com/depromeet/team6/presentation/ui/home/component/FinishCourseDetailButton.kt
@@ -14,8 +14,10 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.depromeet.team6.R
 import com.depromeet.team6.ui.theme.LocalTeam6Colors
 import com.depromeet.team6.ui.theme.LocalTeam6Typography
 
@@ -50,14 +52,14 @@ fun FinishCourseDetailButton(
             shape = RoundedCornerShape(10.dp)
         ) {
             Text(
-                text = "종료",
+                text = stringResource(R.string.home_finish_button_text),
                 style = typography.bodyMedium15,
                 modifier = Modifier.padding(vertical = 14.dp)
             )
         }
 
         CourseDetailButton(
-            text = "경로 상세",
+            text = stringResource(R.string.home_course_detail_button_text),
             onClick = onCourseDetailClick,
             modifier = modifier
         )
