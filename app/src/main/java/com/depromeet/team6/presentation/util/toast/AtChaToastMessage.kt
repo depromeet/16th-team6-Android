e5841a08450dc16afe0cb48a3ab389e17d0651d8
t1nm1ksun <minsuk07672@naver.com>
2025-03-27 13:12:17 +0900

    feat#61 : 커스텀 토스트 메시지 사라질 때 fade out 적용

--- app/src/main/java/com/depromeet/team6/presentation/util/toast/AtChaToastMessage.kt
+++ app/src/main/java/com/depromeet/team6/presentation/util/toast/AtChaToastMessage.kt
@@ -3,6 +3,7 @@ package com.depromeet.team6.presentation.util.toast
 import android.content.Context
 import android.view.Gravity
 import android.view.LayoutInflater
+import android.view.animation.AlphaAnimation
 import android.widget.FrameLayout
 import android.widget.TextView
 import android.widget.Toast
@@ -12,6 +13,9 @@ import com.depromeet.team6.R
 fun atChaToastMessage(context: Context, @StringRes messageResId: Int, length: Int = Toast.LENGTH_SHORT) {
     val layoutInflater = LayoutInflater.from(context)
     val layout = layoutInflater.inflate(R.layout.atcha_toast, null)
+
+    val animationStartTime = length - 500L
+    val animationDuration = 500L
 
     val textView = layout.findViewById<TextView>(R.id.atcha_toast_Message)
     textView.text = context.getString(messageResId)
@@ -26,6 +30,19 @@ fun atChaToastMessage(context: Context, @StringRes messageResId: Int, length: In
         view = toastContainer
     }
 
-    toast.setGravity(Gravity.TOP or Gravity.FILL_HORIZONTAL, 0, 12)
+    val yOffsetDp = 12 // 원하는 dp 값
+    val yOffsetPx = (yOffsetDp * context.resources.displayMetrics.density).toInt()
+    toast.setGravity(Gravity.TOP or Gravity.FILL_HORIZONTAL, 0, yOffsetPx)
+
+
+    // Fade out 애니메이션
+    val fadeOut = AlphaAnimation(1f, 0f).apply {
+        startOffset = animationStartTime
+        duration = animationDuration
+        fillAfter = true
+    }
+
+    toast.view?.startAnimation(fadeOut)
+
     toast.show()
 }
