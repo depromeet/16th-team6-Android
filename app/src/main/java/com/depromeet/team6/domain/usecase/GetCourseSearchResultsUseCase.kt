ab13334e69ca88beb937c21e5ac7787199654cf0
hyuns <hyuns6677@gmail.com>
2025-03-29 06:41:43 +0900

    chore#62 : linting

--- app/src/main/java/com/depromeet/team6/domain/usecase/GetCourseSearchResultsUseCase.kt
+++ app/src/main/java/com/depromeet/team6/domain/usecase/GetCourseSearchResultsUseCase.kt
@@ -3,7 +3,6 @@ package com.depromeet.team6.domain.usecase
 import com.depromeet.team6.data.repositoryimpl.CourseSearchRepositoryImpl
 import com.depromeet.team6.domain.model.Address
 import com.depromeet.team6.domain.model.course.CourseInfo
-import timber.log.Timber
 import javax.inject.Inject
 
 class GetCourseSearchResultsUseCase @Inject constructor(
