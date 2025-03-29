fd154a72f6ce6841865871a493e888a5f1e4ae08
Jieun <lje2000lje@naver.com>
2025-03-29 22:28:18 +0900

    feat/#57 : SearchLocationContract 변수 추가 및 변경

--- app/src/main/java/com/depromeet/team6/presentation/ui/searchlocation/SearchLocationContract.kt
+++ app/src/main/java/com/depromeet/team6/presentation/ui/searchlocation/SearchLocationContract.kt
@@ -1,31 +1,29 @@
 package com.depromeet.team6.presentation.ui.searchlocation
 
-import com.depromeet.team6.domain.model.HomeSearchLocation
+import com.depromeet.team6.domain.model.Location
 import com.depromeet.team6.presentation.util.base.UiEvent
 import com.depromeet.team6.presentation.util.base.UiSideEffect
 import com.depromeet.team6.presentation.util.base.UiState
+import com.depromeet.team6.presentation.util.view.LoadState
 
 class SearchLocationContract {
     data class SearchLocationUiState(
+        val loadState: LoadState = LoadState.Idle,
         val searchQuery: String = "",
-        val searchResults: List<HomeSearchLocation> = emptyList(),
-        val recentSearches: List<HomeSearchLocation> = listOf(
-            HomeSearchLocation(
-                "60계 치킨 강남점",
-                "100m",
-                "강남구 테헤란 4길 6ㅁ"
-            )
-        )
+        val searchResults: List<Location> = emptyList(),
+        val recentSearches: List<Location> = emptyList()
     ) : UiState
 
-    sealed interface SearchLocationSideEffect : UiSideEffect
+    sealed interface SearchLocationSideEffect : UiSideEffect {
+        data object NavigateBack: SearchLocationSideEffect
+    }
 
     sealed class SearchLocationEvent : UiEvent {
-        data class UpdateSearchQuery(val query: String) : SearchLocationEvent()
-        data class UpdateSearchResults(val results: List<HomeSearchLocation>) :
+        data class UpdateSearchQuery(val text: String, val lat: Double, val lon: Double) : SearchLocationEvent()
+        data class UpdateSearchResults(val results: List<Location>) :
             SearchLocationEvent()
 
-        data class UpdateRecentSearches(val recentSearches: List<HomeSearchLocation>) :
+        data class UpdateRecentSearches(val recentSearches: List<Location>) :
             SearchLocationEvent()
 
         data object ClearRecentSearches : SearchLocationEvent()
