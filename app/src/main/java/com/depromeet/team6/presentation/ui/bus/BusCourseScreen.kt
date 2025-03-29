a55caa038c0961f3e00afc1af920d7b0b0dc7ee3
t1nm1ksun <minsuk07672@naver.com>
2025-03-29 16:09:34 +0900

    feat#72 : 버스 정보 아이템 1차 구현

---
+++ app/src/main/java/com/depromeet/team6/presentation/ui/bus/BusCourseScreen.kt
@@ -0,0 +1,15 @@
+package com.depromeet.team6.presentation.ui.bus
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun BusCourseScreen(modifier: Modifier = Modifier) {
+}
+
+@Preview
+@Composable
+private fun BusCourseScreenPreview() {
+    BusCourseScreen()
+}
---
+++ app/src/main/java/com/depromeet/team6/presentation/ui/bus/BusCourseViewModel.kt
@@ -0,0 +1,1 @@
+package com.depromeet.team6.presentation.ui.bus
