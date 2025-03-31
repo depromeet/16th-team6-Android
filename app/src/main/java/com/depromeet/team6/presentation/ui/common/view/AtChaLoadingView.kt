3d5ee28190f35f9c762a6a94ce7e03774487eb09
t1nm1ksun <minsuk07672@naver.com>
2025-03-31 17:20:46 +0900

    feat#72 : 로딩 뷰 구현

---
+++ app/src/main/java/com/depromeet/team6/presentation/ui/common/view/AtChaLoadingView.kt
@@ -0,0 +1,28 @@
+package com.depromeet.team6.presentation.ui.common.view
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.depromeet.team6.ui.theme.LocalTeam6Colors
+import com.depromeet.team6.ui.theme.defaultTeam6Colors
+
+@Composable
+fun AtChaLoadingView() {
+    Box(
+        modifier = Modifier.fillMaxSize().background(color = defaultTeam6Colors.greyWashBackground),
+        contentAlignment = Alignment.Center
+    ) {
+       Text(text = "Loading...", color = LocalTeam6Colors.current.white)
+    }
+}
+
+@Preview
+@Composable
+fun AtChaLoadingViewPreview() {
+    AtChaLoadingView()
+}
