df7d506ebdc16d3446f43ed546c27438947346ca
cyjadela <adelacho@naver.com>
2025-04-02 20:35:31 +0900

    chore/#63 : lint 적용

--- app/src/main/java/com/depromeet/team6/data/datalocal/service/LockService.kt
+++ app/src/main/java/com/depromeet/team6/data/datalocal/service/LockService.kt
@@ -1,7 +1,6 @@
 package com.depromeet.team6.data.datalocal.service
 
 import android.app.Service
-import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
 import android.media.MediaPlayer
