11eb73630239c6f96f2ef7f6e2c8b6f68f43696c
cyjadela <adelacho@naver.com>
2025-04-12 18:44:41 +0900

    feat/#113 : 마이페이지 우리 집 수정 API (domain layer) 연동

---
+++ app/src/main/java/com/depromeet/team6/domain/usecase/ModifyUserInfoUseCase.kt
@@ -0,0 +1,14 @@
+package com.depromeet.team6.domain.usecase
+
+import com.depromeet.team6.data.dataremote.model.request.user.RequestModifyUserInfoDto
+import com.depromeet.team6.domain.model.UserInfo
+import com.depromeet.team6.domain.repository.AuthRepository
+import javax.inject.Inject
+
+class ModifyUserInfoUseCase @Inject constructor(
+    private val authRepository: AuthRepository
+) {
+    suspend operator fun invoke(modifyUserInfoDto: RequestModifyUserInfoDto): Result<UserInfo> =
+        authRepository.modifyUserInfo(modifyUserInfoDto = modifyUserInfoDto)
+
+}
