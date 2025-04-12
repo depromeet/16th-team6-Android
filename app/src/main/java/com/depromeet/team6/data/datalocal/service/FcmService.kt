810ded2732d7aeeaa8eb6c87eab03ec69bcdaa11
cyjadela <adelacho@naver.com>
2025-04-12 10:51:39 +0900

    chore/#110 : ktlint 적용

--- app/src/main/java/com/depromeet/team6/data/datalocal/service/FcmService.kt
+++ app/src/main/java/com/depromeet/team6/data/datalocal/service/FcmService.kt
@@ -6,11 +6,8 @@ import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
-import android.media.RingtoneManager
-import android.os.Build
 import android.os.PowerManager
 import android.util.Log
-import androidx.compose.ui.res.stringResource
 import androidx.core.app.NotificationCompat
 import androidx.core.content.ContextCompat
 import com.depromeet.team6.R
@@ -129,7 +126,9 @@ class FcmService : FirebaseMessagingService() {
             flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TOP
         }
         val pendingIntent = PendingIntent.getActivity(
-            this, 0, intent,
+            this,
+            0,
+            intent,
             PendingIntent.FLAG_IMMUTABLE
         )
 
