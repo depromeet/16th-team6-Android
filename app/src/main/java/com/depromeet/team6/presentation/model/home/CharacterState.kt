3cdbb282ab70389105088837955104e7bf3fade6
cyjadela <adelacho@naver.com>
2025-06-12 15:08:12 +0900

    feat/#140 : 애니메이션 조건 State 생성

---
+++ app/src/main/java/com/depromeet/team6/presentation/model/home/CharacterState.kt
@@ -0,0 +1,29 @@
+package com.depromeet.team6.presentation.model.home
+
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import com.depromeet.team6.R
+
+data class CharacterState(
+    val speechTexts: List<SpeechBubbleData> = emptyList(),
+    val currentSpeechIndex: Int = 0,
+    val lottieResId: Int = R.raw.atcha_character_1,
+    val isAnimating: Boolean = false,
+    val bottomPadding: Dp = 194.dp
+)
+
+data class SpeechBubbleData(
+    val prefixText: String = "",
+    val emphasisText: String? = null,
+    val suffixText: String? = null,
+    val topPrefixText: String? = null,
+    val topEmphasisText: String? = null,
+    val topSuffixText: String? = null,
+    val lineCount: Int = 1
+)
+
+enum class ComponentType {
+    DEPARTURE_TIME_NOT_CONFIRMED_CLICKED,
+    DEPARTURE_TIME_CONFIRMED_CLICKED,
+    ROUTE_TEXT_CLICKED
+}
