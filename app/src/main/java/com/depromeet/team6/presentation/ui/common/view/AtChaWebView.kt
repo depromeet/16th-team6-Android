38f59548733f2412fb6a651674e072cd06daa840
t1nm1ksun <minsuk07672@naver.com>
2025-06-27 00:44:38 +0900

    chore/#150 : ktlint Format

--- app/src/main/java/com/depromeet/team6/presentation/ui/common/view/AtChaWebView.kt
+++ app/src/main/java/com/depromeet/team6/presentation/ui/common/view/AtChaWebView.kt
@@ -4,7 +4,6 @@ import android.webkit.WebViewClient
 import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
