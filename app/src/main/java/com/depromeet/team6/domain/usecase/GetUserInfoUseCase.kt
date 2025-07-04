2127466c781a52d465075523ad8725d69f742631
cyjadela <adelacho@naver.com>
2025-07-04 09:45:24 +0900

    chore/#155 : 디스코드 알림 에러 처리 제거

--- app/src/main/java/com/depromeet/team6/domain/usecase/GetUserInfoUseCase.kt
+++ app/src/main/java/com/depromeet/team6/domain/usecase/GetUserInfoUseCase.kt
@@ -22,8 +22,6 @@ class GetUserInfoUseCase @Inject constructor(
 
     override fun apiExceptionMapper(errorCode: String): ErrorControlFailureException {
         return when (errorCode) {
-            "REQ_001" -> ErrorControlFailureException.ShowToastException("알 수 없는 서버 에러입니다.") // TODO : 디코 알림 전송
-            "REQ_002" -> ErrorControlFailureException.ShowToastException("알 수 없는 서버 에러입니다.") // TODO : 디코 알림 전송
             "TOK_001" -> ErrorControlFailureException.NavigateAndShowToastException("로그인 정보가 만료되었습니다. 다시 로그인 해주세요.", Route.Login)
             "TOK_002" -> ErrorControlFailureException.NavigateAndShowToastException("로그인 정보가 만료되었습니다. 다시 로그인 해주세요.", Route.Login)
             "USR_002" -> ErrorControlFailureException.NavigateAndShowToastException("로그인 정보가 만료되었습니다. 다시 로그인 해주세요.", Route.Login)
