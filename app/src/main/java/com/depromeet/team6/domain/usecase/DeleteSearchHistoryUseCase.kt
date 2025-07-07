a2da34056467d6fd7f2db330f6748c9e6d2a1625
Jieun <lje2000lje@naver.com>
2025-07-08 00:13:29 +0900

    feat/#158 : 최근 검색 내역 삭제 API 에러 핸들링

--- app/src/main/java/com/depromeet/team6/domain/usecase/DeleteSearchHistoryUseCase.kt
+++ app/src/main/java/com/depromeet/team6/domain/usecase/DeleteSearchHistoryUseCase.kt
@@ -1,13 +1,54 @@
 package com.depromeet.team6.domain.usecase
 
-import com.depromeet.team6.domain.model.SearchHistory
+import com.depromeet.team6.domain.Auth.TOK_001
+import com.depromeet.team6.domain.Auth.TOK_002
+import com.depromeet.team6.domain.Auth.USR_002
+import com.depromeet.team6.domain.Network.INTERNAL_SERVER_ERROR
+import com.depromeet.team6.domain.ToastMessage.API_ERROR_LOGIN_TOKEN_EXPIRED
+import com.depromeet.team6.domain.ToastMessage.API_ERROR_NETWORK_FAILURE
 import com.depromeet.team6.domain.repository.LocationsRepository
-import retrofit2.Response
+import com.depromeet.team6.domain.usecase.base.ApiRequestUseCase
+import com.depromeet.team6.presentation.model.exception.ErrorControlFailureException
+import com.depromeet.team6.presentation.model.route.Route
 import javax.inject.Inject
 
 class DeleteSearchHistoryUseCase @Inject constructor(
     private val locationsRepository: LocationsRepository
-) {
-    suspend operator fun invoke(searchHistory: SearchHistory): Response<Unit> =
-        locationsRepository.deleteSearchHistory(name = searchHistory.name, lat = searchHistory.lat, lon = searchHistory.lon, businessCategory = searchHistory.businessCategory, address = searchHistory.address)
+) : ApiRequestUseCase<DeleteSearchHistoryUseCase.Params, Unit>() {
+
+    data class Params(
+        val name: String,
+        val lat: Double,
+        val lon: Double,
+        val businessCategory: String,
+        val address: String
+    )
+
+    suspend operator fun invoke(
+        name: String,
+        lat: Double,
+        lon: Double,
+        businessCategory: String,
+        address: String
+    ): Result<Unit> = invoke(Params(name, lat, lon, businessCategory, address))
+
+    override suspend fun apiCall(params: Params): Result<Unit> {
+        return locationsRepository.deleteSearchHistory(
+            name = params.name,
+            lat = params.lat,
+            lon = params.lon,
+            businessCategory = params.businessCategory,
+            address = params.address
+        )
+    }
+
+    override fun apiExceptionMapper(errorCode: String): ErrorControlFailureException {
+        return when(errorCode) {
+            TOK_001 -> ErrorControlFailureException.ShowToastException(toastMessage = API_ERROR_LOGIN_TOKEN_EXPIRED)
+            TOK_002 -> ErrorControlFailureException.ShowToastException(toastMessage = API_ERROR_LOGIN_TOKEN_EXPIRED)
+            USR_002 -> ErrorControlFailureException.NavigateAndShowToastException(toastMessage = API_ERROR_LOGIN_TOKEN_EXPIRED, route = Route.Login)
+            INTERNAL_SERVER_ERROR -> ErrorControlFailureException.ShowToastException(toastMessage = API_ERROR_NETWORK_FAILURE)
+            else -> ErrorControlFailureException.ShowToastException(API_ERROR_NETWORK_FAILURE)
+        }
+    }
 }
