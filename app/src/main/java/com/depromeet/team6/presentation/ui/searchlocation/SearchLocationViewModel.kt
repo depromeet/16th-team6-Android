2d7d80d4073a5e8acfeb50b5f868aec7f8be5518
Jieun <lje2000lje@naver.com>
2025-07-08 00:14:15 +0900

    feat/#158 : 최근 검색 내역 관련 viewModel API 에러 핸들링

--- app/src/main/java/com/depromeet/team6/presentation/ui/searchlocation/SearchLocationViewModel.kt
+++ app/src/main/java/com/depromeet/team6/presentation/ui/searchlocation/SearchLocationViewModel.kt
@@ -114,19 +114,29 @@ class SearchLocationViewModel @Inject constructor(
         )
 
         viewModelScope.launch {
-            if (deleteSearchHistoryUseCase(searchHistory = convertedSearchHistory).isSuccessful) {
-                updateRecentSearches(location = LatLng(location.latitude, location.longitude))
+            deleteSearchHistoryUseCase(
+                name = searchHistory.name,
+                lat = searchHistory.lat,
+                lon = searchHistory.lon,
+                businessCategory = searchHistory.businessCategory,
+                address = searchHistory.address
+            )
+                .onSuccess {
+                    updateRecentSearches(location = LatLng(location.latitude, location.longitude))
 
-                setEvent(
-                    SearchLocationContract.SearchLocationEvent.DeleteSearchHistory(
-                        searchHistory = convertedSearchHistory
+                    setEvent(
+                        SearchLocationContract.SearchLocationEvent.DeleteSearchHistory(
+                            searchHistory = convertedSearchHistory
+                        )
                     )
-                )
-            } else {
-                Timber.e("deleteSearchHistory failure")
-            }
+                }
+                .onFailure { exception ->
+                    Timber.e("deleteSearchHistory failure")
+                    handleApiException(exception)
+                }
         }
     }
+
 
     // 검색 내역 서버에 전송
     fun postSearchHistory(searchHistory: Location) {
@@ -139,22 +149,26 @@ class SearchLocationViewModel @Inject constructor(
         )
 
         viewModelScope.launch {
-            if (postSearchHistoriesUseCase(searchHistory = convertedSearchHistory).isSuccessful) {
-                Timber.e("postSearchHistory Success")
-            } else {
-                Timber.e("postSearchHistory failure")
-            }
+            postSearchHistoriesUseCase(searchHistory = convertedSearchHistory)
+                .onSuccess {
+                    Timber.e("postSearchHistory Success")
+                }
+                .onFailure { exception ->
+                    handleApiException(exception)
+                }
         }
     }
 
     // 최근 검색 내역 전체 삭제
     fun deleteAllSearchHistory() {
         viewModelScope.launch {
-            if (deleteAllSearchHistoryUseCase().isSuccessful) {
+            deleteAllSearchHistoryUseCase().onSuccess {
                 setEvent(SearchLocationContract.SearchLocationEvent.ClearRecentSearches)
-            } else {
-                Timber.e("deleteAllSearchHistory failure")
+
             }
+                .onFailure { exception ->
+                    handleApiException(exception)
+                }
         }
     }
 
