82e0eed4699009cf0479f88902a77227aced15d4
Jieun <lje2000lje@naver.com>
2025-07-11 23:00:37 +0900

    fix/#181 : 현위치 버튼 위치 변경

--- app/src/main/java/com/depromeet/team6/presentation/ui/home/component/MapView.kt
+++ app/src/main/java/com/depromeet/team6/presentation/ui/home/component/MapView.kt
@@ -152,9 +152,9 @@ fun TMapViewCompose(
                     .align(Alignment.BottomEnd)
                     .then(
                         if (isAlarmRegistered) {
-                            Modifier.padding(bottom = 35.dp, end = 16.dp)
+                            Modifier.padding(bottom = 55.dp, end = 16.dp)
                         } else {
-                            Modifier.padding(bottom = 35.dp, end = 16.dp)
+                            Modifier.padding(bottom = 55.dp, end = 16.dp)
                         }
                     )
                     .clickable(enabled = isMapReady) {
