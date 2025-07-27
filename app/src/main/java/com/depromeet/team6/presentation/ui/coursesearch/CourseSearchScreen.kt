15e9c8401d63f682f42f66480c7f15b4b099b91f
hyuns <hyuns6677@gmail.com>
2025-07-27 22:56:40 +0900

    feat/#177 : 로딩뷰 수정
    - 경로 검색 화면에서 기본 로딩뷰 -> 앗차 로딩뷰 나오도록 수정했습니다.

--- app/src/main/java/com/depromeet/team6/presentation/ui/coursesearch/CourseSearchScreen.kt
+++ app/src/main/java/com/depromeet/team6/presentation/ui/coursesearch/CourseSearchScreen.kt
@@ -8,7 +8,6 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.LaunchedEffect
@@ -26,6 +25,7 @@ import androidx.lifecycle.compose.LocalLifecycleOwner
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.depromeet.team6.R
 import com.depromeet.team6.domain.model.course.LegInfo
+import com.depromeet.team6.presentation.ui.common.view.AtChaLoadingView
 import com.depromeet.team6.presentation.ui.coursesearch.component.CourseAppBar
 import com.depromeet.team6.presentation.ui.coursesearch.component.DestinationSearchBar
 import com.depromeet.team6.presentation.ui.coursesearch.component.TransportTabMenu
@@ -134,7 +134,7 @@ fun CourseSearchRoute(
                     .padding(padding)
             )
             Box(modifier = Modifier.fillMaxSize()) {
-                CircularProgressIndicator(modifier = Modifier.align(Alignment.Center))
+                AtChaLoadingView()
             }
         }
 
