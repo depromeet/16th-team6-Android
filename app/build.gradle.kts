a1e7dfa7d6b90c186f80d973911705a979cf8083
hyuns <hyuns6677@gmail.com>
2025-08-19 20:52:57 +0900

    chore : version up

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,8 +39,8 @@ android {
         applicationId = "com.depromeet.team6"
         minSdk = 26
         targetSdk = 35
-        versionCode = 18
-        versionName = "1.3.12"
+        versionCode = 19
+        versionName = "1.3.13"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         buildConfigField("String", "KAKAO_NATIVE_APP_KEY", properties["kakao.native.app.key"].toString())
