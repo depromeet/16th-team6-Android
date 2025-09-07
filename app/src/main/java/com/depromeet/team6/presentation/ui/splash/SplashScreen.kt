8584bf6ee3cae820436fc1430df577fc602b262a
t1nm1ksun <minsuk07672@naver.com>
2025-09-07 17:45:14 +0900

    chore/#230: ktlintFormat

--- app/src/main/java/com/depromeet/team6/presentation/ui/splash/SplashScreen.kt
+++ app/src/main/java/com/depromeet/team6/presentation/ui/splash/SplashScreen.kt
@@ -2,10 +2,7 @@ package com.depromeet.team6.presentation.ui.splash
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
 import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -14,7 +11,6 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.depromeet.team6.R
 import com.depromeet.team6.ui.theme.Team6Theme
 
@@ -26,11 +22,11 @@ fun SplashScreen(modifier: Modifier = Modifier) {
             .background(color = Team6Theme.colors.black),
         contentAlignment = Alignment.Center
     ) {
-            Icon(
-                imageVector = ImageVector.vectorResource(R.drawable.ic_splash_app_logo),
-                contentDescription = null,
-                tint = Color.Unspecified,
-            )
+        Icon(
+            imageVector = ImageVector.vectorResource(R.drawable.ic_splash_app_logo),
+            contentDescription = null,
+            tint = Color.Unspecified
+        )
     }
 }
 
