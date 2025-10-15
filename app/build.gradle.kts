620d3747322b6e73ea22ed7f24424861e20efeb0
hyuns <hyuns6677@gmail.com>
2025-10-15 15:51:17 +0900

    fix/#234 : 16KB 페이지 메모리 지원 업데이트 & 버전 업
    - 안드로이드 신규 OS 정책 업데이트에 따라 16KB 페이지 메모리를 지원하도록 수정했습니다.
    - TmapSDK를 신규 버전으로 업데이트 하여 16KB 미지원 이슈를 해결했습니다.
    - 플레이스토어 배포를 위해 버전을 올렸습니다.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,8 +39,8 @@ android {
         applicationId = "com.depromeet.team6"
         minSdk = 26
         targetSdk = 35
-        versionCode = 19
-        versionName = "1.3.13"
+        versionCode = 20
+        versionName = "1.3.14"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         buildConfigField("String", "KAKAO_NATIVE_APP_KEY", properties["kakao.native.app.key"].toString())
@@ -143,8 +143,8 @@ dependencies {
     implementation(libs.androidx.security.crypto)
 
     // Tmap
-    implementation(files("libs/tmap-sdk-1.9.aar"))
-    implementation(files("libs/vsm-tmap-sdk-v2-android-1.7.23.aar"))
+    implementation(files("libs/tmap-sdk-3.0.aar"))
+    implementation(files("libs/vsm-tmap-sdk-v2-android-1.7.45.aar"))
     implementation(libs.flatbuffers.java)
 
     // Firebase
