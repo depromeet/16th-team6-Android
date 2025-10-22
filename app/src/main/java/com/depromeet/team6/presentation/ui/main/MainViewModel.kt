d56ebdbfc7a4a2d67b3fea66c27158b840915c79
hyuns <hyuns6677@gmail.com>
2025-10-22 23:43:17 +0900

    fix/#233 : 알람 동작시 진동여부 확인 로직 수정

--- app/src/main/java/com/depromeet/team6/presentation/ui/main/MainViewModel.kt
+++ app/src/main/java/com/depromeet/team6/presentation/ui/main/MainViewModel.kt
@@ -179,8 +179,6 @@ class MainViewModel @Inject constructor(
         this.replace("[^0-9.]".toRegex(), "")
 
     private fun compareVersions(installed: String, latest: String): VersionResult {
-        if (com.depromeet.team6.BuildConfig.DEBUG) return VersionResult.UP_TO_DATE
-
         val installedParts = installed.split(".").map { it.toIntOrNull() ?: 0 }
         val latestParts = latest.split(".").map { it.toIntOrNull() ?: 0 }
 
