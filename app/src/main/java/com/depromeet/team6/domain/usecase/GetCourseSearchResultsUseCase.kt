c9e892d1ac00c209c3c711247f864d187996ec28
hyuns <hyuns6677@gmail.com>
2026-04-18 02:34:41 +0900

    feat: ErrorControlFailureException 기반 코스 검색 에러 처리 로직 리팩터링

--- app/src/main/java/com/depromeet/team6/domain/usecase/GetCourseSearchResultsUseCase.kt
+++ app/src/main/java/com/depromeet/team6/domain/usecase/GetCourseSearchResultsUseCase.kt
@@ -1,9 +1,12 @@
 package com.depromeet.team6.domain.usecase
 
+import com.depromeet.team6.data.dataremote.model.response.base.ApiException
 import com.depromeet.team6.data.repositoryimpl.TransitsRepositoryImpl
 import com.depromeet.team6.domain.model.Address
 import com.depromeet.team6.domain.model.course.CourseInfo
+import com.depromeet.team6.presentation.model.exception.ErrorControlFailureException
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
 import javax.inject.Inject
 
 class GetCourseSearchResultsUseCase @Inject constructor(
@@ -18,24 +21,20 @@ class GetCourseSearchResultsUseCase @Inject constructor(
             startPosition = startPoint,
             endPosition = endPoint,
             sortType = sortType
-        )
+        ).catch { exception ->
+            throw mapException(exception)
+        }
     }
 
-//    override fun apiExceptionMapper(errorCode: String): ErrorControlFailureException {
-//        return when (errorCode) {
-//            "REQ_001" -> TODO()
-//            "REQ_002" -> TODO()
-//            "TOK_002" -> ErrorControlFailureException.NavigateAndShowToastException(toastMessage = API_ERROR_LOGIN_TOKEN_EXPIRED, route = Route.Login)
-//            "USR_002" -> ErrorControlFailureException.NavigateAndShowToastException(toastMessage = API_ERROR_INVALID_LOCATION, route = Route.Back)
-//            "LOC_003" -> ErrorControlFailureException.NavigateAndShowToastException(toastMessage = API_ERROR_INVALID_LOCATION, route = Route.Back)
-//            "LOC_004" -> ErrorControlFailureException.NavigateAndShowToastException(toastMessage = API_ERROR_INVALID_LOCATION, route = Route.Back)
-//            "TRS_001" -> TODO()
-//            "TRS_011" -> ErrorControlFailureException.NavigateAndShowToastException(toastMessage = API_ERROR_SHORT_DISTANCE, route = Route.Back)
-//            "TRS_012" -> ErrorControlFailureException.NavigateAndShowToastException(toastMessage = API_ERROR_OUT_OF_SERVICE_REGION, route = Route.Back)
-//            "LRT_003" -> ErrorControlFailureException.SetUIStateException(errorCode = "LRT_003")
-//            "LRT_004" -> ErrorControlFailureException.SetUIStateException(errorCode = "LRT_004")
-//            "INTERNAL_SERVER_ERROR" -> ErrorControlFailureException.ShowToastException(toastMessage = API_ERROR_NETWORK_FAILURE)
-//            else -> ErrorControlFailureException.ShowToastException(API_ERROR_NETWORK_FAILURE)
-//        }
-//    }
+    private fun mapException(exception: Throwable): ErrorControlFailureException =
+        when (exception) {
+            is ApiException.ApiRequestFailureException -> when (exception.errorCode) {
+                "LRT_003", "LRT_004" -> ErrorControlFailureException.SetUIStateException(exception.errorCode)
+                else -> ErrorControlFailureException.ShowToastException(exception.errorMessage)
+            }
+            is ApiException.NetworkFailureException ->
+                ErrorControlFailureException.ShowToastException(exception.errorMessage)
+            else ->
+                ErrorControlFailureException.ShowToastException("알 수 없는 오류가 발생했습니다.")
+        }
 }
